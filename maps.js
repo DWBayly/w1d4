9c4b60242dddc7f5e180633320e54839f0e7fa7d
dbaylyuvic <dbayly@uvic.ca>
2017-11-02 10:15:09 -0700

    Created a map function with test cases

---
+++ maps.js
@@ -0,0 +1,24 @@
+function map(arr,fun){
+	arr.forEach(function(item,index){
+		//console.log(fun(item));
+		arr[index] = fun(item);
+		
+	});
+}
+//arr = [6, 7, 2, 5, 3];
+//arr =[ "GROUND", "CONTROL", "TO", "MAJOR", "TOM" ];
+arr =[ 'dnuorg', 'lortnoc', 'ot', 'rojam', 'mot' ];
+var fun = function(x){
+	//return x + 1;
+	//console.log(x);
+	return x.toUpperCase();
+	//return x.split('').reverse().join('');
+}
+console.log(arr);
+map(arr,fun);
+console.log(arr);
+
+
+//
+
+//
