612498ddb4be82e2d6f69f9406a6f36f2b82889d
dbaylyuvic <dbayly@uvic.ca>
2017-11-02 09:57:32 -0700

    added sorting students to sorting.js

--- sorting.js
+++ sorting.js
@@ -5,3 +5,18 @@ arr.sort(function(a,b){
 	
 });
 console.log(arr);
+
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+students.sort(function(a,b){
+	if(a.id== b.id){
+			return a.value>b.value;
+	}else{
+		return a.name>b.name;
+	}
+});
+console.log(students);
